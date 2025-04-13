1d839190caded4ea4f171c328c5e01734623212e
jlsare27 <163943988+jlsare27@users.noreply.github.com>
2025-04-13 17:49:50 -0600

    Create StoreLightingRequest

---
+++ app/Http/Requests/StoreLightingRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreLightingRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'type' => 'required|in:incandescente,halogena,fluorescente,LED,otra',
+            'power' => 'required|numeric|min:0.1',
+            'quantity' => 'required|integer|min:1',
+            'hours_use' => 'required|numeric|min:0|max:24',
+            'location' => 'nullable|string|max:100',
+        ];
+    }
+}
